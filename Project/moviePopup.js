4a394937b2314f35d9979c69ffec9e5a5778f445
Vyrtz <56693735+Vyrtz@users.noreply.github.com>
2019-11-12 14:48:38 -0600

    Add files via upload

---
+++ Project/moviePopup.js
@@ -0,0 +1,9 @@
+function popup() {
+    Swal.fire({
+        title: "Select Showtime",
+        text: "please select desired showtime",
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+        imageHeight: 800,
+        button: "confirm"
+      });
+}
