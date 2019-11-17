b61973b16a12608a58b7b789a75a25795da594c7
brogrammer39 <lungalb@myumanitoba.ca>
2019-11-17 16:07:05 -0600

    main page prototype complete

--- moviePopup.js
+++ moviePopup.js
@@ -1,24 +1,14 @@
-  /*
-function popup() {
-    Swal.fire({
-        title: "Select Showtime",
-        text: "please select desired showtime",
-        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
-        imageHeight: 800,
-        button: "confirm"
-      });
-}
-*/
 
 //get modal element
 var modal = document.getElementById('movieModal');
 var poster = document.getElementById('poster1');
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
+var contBtn = document.getElementById('contBtn');
 var timeSelect1 = document.getElementById('130Btn');
 var timeSelect2 = document.getElementById('345Btn');
 var timeSelect3 = document.getElementById('630Btn');
 var timeSelect4 = document.getElementById('900Btn');
-
+var thePoster = document.getElementById('modalPoster');
 var selectedTime = "-1";
 
 //listen for open click
@@ -35,6 +25,8 @@ timeSelect2.addEventListener('click', selected2);
 timeSelect3.addEventListener('click', selected3);
 timeSelect4.addEventListener('click', selected4);
 
+contBtn.addEventListener('click',continueClick);
+
 function openModal(){
   modal.style.display = 'block';
 }
@@ -49,8 +41,15 @@ function clickOutside(e){
   } 
 }
 
+function continueClick(){
+  modal.style.display = 'none';
+}
+
 function selected1(){
   timeSelect1.style.border = 'black 2px solid';
+  //for dynamic popup
+  //modalPoster.src='./img/brPoster.jpg';
+  //document.getElementById('modalTitle').innerText = "Blade Runner";
   selectedTime = "1:30";
   timeSelect2.style.border = 'none';
   timeSelect3.style.border = 'none';
