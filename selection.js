30c37f4aca7875e4da47bec02d211ed141dba9ad
brogrammer39 <lungalb@myumanitoba.ca>
2019-11-18 17:51:47 -0600

    seat selection almost done

---
+++ selection.js
@@ -0,0 +1,163 @@
+var selectedSeat = "00";
+
+var seat0 = document.getElementsByClassName('grid-item')[0];
+var seat1 = document.getElementsByClassName('grid-item')[1];
+var seat2 = document.getElementsByClassName('grid-item')[2];
+var seat3 = document.getElementsByClassName('grid-item')[3];
+var seat4 = document.getElementsByClassName('grid-item')[4];
+var seat5 = document.getElementsByClassName('grid-item')[5];
+var seat6 = document.getElementsByClassName('grid-item')[6];
+var seat7 = document.getElementsByClassName('grid-item')[7];
+var seat8 = document.getElementsByClassName('grid-item')[8];
+var seat9 = document.getElementsByClassName('grid-item')[9];
+var seat10 = document.getElementsByClassName('grid-item')[10];
+var seat11 = document.getElementsByClassName('grid-item')[11];
+var seat12 = document.getElementsByClassName('grid-item')[12];
+var seat13 = document.getElementsByClassName('grid-item')[13];
+var seat14 = document.getElementsByClassName('grid-item')[14];
+var seat15 = document.getElementsByClassName('grid-item')[15];
+var seat16 = document.getElementsByClassName('grid-item')[16];
+var seat17 = document.getElementsByClassName('grid-item')[17];
+var seat18 = document.getElementsByClassName('grid-item')[18];
+var seat19 = document.getElementsByClassName('grid-item')[19];
+var seat20 = document.getElementsByClassName('grid-item')[20];
+var seat21 = document.getElementsByClassName('grid-item')[21];
+var seat22 = document.getElementsByClassName('grid-item')[22];
+var seat23 = document.getElementsByClassName('grid-item')[23];
+var seat24 = document.getElementsByClassName('grid-item')[24];
+var seat25 = document.getElementsByClassName('grid-item')[25];
+var seat26 = document.getElementsByClassName('grid-item')[26];
+var seat27 = document.getElementsByClassName('grid-item')[27];
+var seat28 = document.getElementsByClassName('grid-item')[28];
+var seat29 = document.getElementsByClassName('grid-item')[29];
+var seat30 = document.getElementsByClassName('grid-item')[30];
+var seat31 = document.getElementsByClassName('grid-item')[31];
+
+seat0.addEventListener('click',function(){
+    selectedSeat = "A1";
+    console.log(selectedSeat);
+});
+seat1.addEventListener('click',function(){
+    selectedSeat = "A2";
+    console.log(selectedSeat);
+});
+seat2.addEventListener('click',function(){
+    selectedSeat = "A3";
+    console.log(selectedSeat);
+});
+seat3.addEventListener('click',function(){
+    selectedSeat = "A4";
+    console.log(selectedSeat);
+});
+seat4.addEventListener('click',function(){
+    selectedSeat = "A5";
+    console.log(selectedSeat);
+});
+seat5.addEventListener('click',function(){
+    selectedSeat = "A6";
+    console.log(selectedSeat);
+});
+seat6.addEventListener('click',function(){
+    selectedSeat = "A7";
+    console.log(selectedSeat);
+});
+seat7.addEventListener('click',function(){
+    selectedSeat = "A8";
+    console.log(selectedSeat);
+});
+seat8.addEventListener('click',function(){
+    selectedSeat = "B1";
+    console.log(selectedSeat);
+});
+seat9.addEventListener('click',function(){
+    selectedSeat = "B2";
+    console.log(selectedSeat);
+});
+seat10.addEventListener('click',function(){
+    selectedSeat = "B3";
+    console.log(selectedSeat);
+});
+seat11.addEventListener('click',function(){
+    selectedSeat = "B4";
+    console.log(selectedSeat);
+});
+seat12.addEventListener('click',function(){
+    selectedSeat = "B5";
+    console.log(selectedSeat);
+});
+seat13.addEventListener('click',function(){
+    selectedSeat = "B6";
+    console.log(selectedSeat);
+});
+seat14.addEventListener('click',function(){
+    selectedSeat = "B7";
+    console.log(selectedSeat);
+});
+seat15.addEventListener('click',function(){
+    selectedSeat = "B8";
+    console.log(selectedSeat);
+});
+seat16.addEventListener('click',function(){
+    selectedSeat = "C1";
+    console.log(selectedSeat);
+});
+seat17.addEventListener('click',function(){
+    selectedSeat = "C2";
+    console.log(selectedSeat);
+});
+seat18.addEventListener('click',function(){
+    selectedSeat = "C3";
+    console.log(selectedSeat);
+});
+seat19.addEventListener('click',function(){
+    selectedSeat = "C4";
+    console.log(selectedSeat);
+});
+seat20.addEventListener('click',function(){
+    selectedSeat = "C5";
+    console.log(selectedSeat);
+});
+seat21.addEventListener('click',function(){
+    selectedSeat = "C6";
+    console.log(selectedSeat);
+});
+seat22.addEventListener('click',function(){
+    selectedSeat = "C7";
+    console.log(selectedSeat);
+});
+seat23.addEventListener('click',function(){
+    selectedSeat = "C8";
+    console.log(selectedSeat);
+});
+seat24.addEventListener('click',function(){
+    selectedSeat = "D1";
+    console.log(selectedSeat);
+});
+seat25.addEventListener('click',function(){
+    selectedSeat = "D2";
+    console.log(selectedSeat);
+});
+seat26.addEventListener('click',function(){
+    selectedSeat = "D3";
+    console.log(selectedSeat);
+});
+seat27.addEventListener('click',function(){
+    selectedSeat = "D4";
+    console.log(selectedSeat);
+});
+seat28.addEventListener('click',function(){
+    selectedSeat = "D5";
+    console.log(selectedSeat);
+});
+seat29.addEventListener('click',function(){
+    selectedSeat = "D6";
+    console.log(selectedSeat);
+});
+seat30.addEventListener('click',function(){
+    selectedSeat = "D7";
+    console.log(selectedSeat);
+});
+seat31.addEventListener('click',function(){
+    selectedSeat = "D8";
+    console.log(selectedSeat);
+});
